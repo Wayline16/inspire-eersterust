c77d33d21f3e896b31ce92e0c04f144be87dca28
Wayline Jeffries <120781316+Wayline16@users.noreply.github.com>
2024-03-06 13:20:34 +0000

    All pages added for navigation

--- app.py
+++ app.py
@@ -4,8 +4,33 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_wayline():
+def home():
   return render_template('home.html')
+
+@app.route("/about")
+def about():
+  return render_template('about.html')
+
+@app.route("/events")
+def events():
+  return render_template('events.html')
+
+@app.route("/contact")
+def contact():
+  return render_template('contact.html')
+
+
+@app.route("/gallery1")
+def gallery1():
+  return render_template('gallery1.html')
+
+@app.route("/gallery2")
+def gallery2():
+  return render_template('gallery2.html')
+
+@app.route("/gallery3")
+def gallery3():
+  return render_template('gallery3.html')
 
 
 if __name__ == "__main__":
